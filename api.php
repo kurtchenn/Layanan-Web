50007bf23012b2604dda02ef8177dc60bf5a8def
Kurt Chen <43069326+kurtchenn@users.noreply.github.com>
2020-04-16 02:58:17 +0700

    Add files via upload

---
+++ api.php
@@ -0,0 +1,10 @@
+<?php
+
+use Illuminate\Http\Request;
+
+include('api_routes/auth_routes.php');
+
+Route::group(['middleware' => 'auth:api'], function(){
+	include('api_routes/user_routes.php');
+
+});
---
+++ product_category_routes.php
@@ -0,0 +1,5 @@
+<?php 
+Route::get('get-product-category','API\ProductCategoryController@index');
+Route::post('add-product-category','API\ProductCategoryController@index');
+Route::put('update-product-category','API\ProductCategoryController@index');
+Route::delete('delete-product-category','API\ProductCategoryController@index');
